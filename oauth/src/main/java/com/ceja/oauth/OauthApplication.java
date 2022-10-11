e980e9834fd36344ca0ba5e6ab22a6c7077965fa
HectorCeja <cejagomezhector@gmail.com>
2022-10-11 13:03:28 -0600

    adding oauth module

---
+++ oauth/src/main/java/com/ceja/oauth/OauthApplication.java
@@ -0,0 +1,18 @@
+package com.ceja.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
